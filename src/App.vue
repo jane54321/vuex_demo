4f20dea8959f84e03293bbce60e86d069bfc83f7
zml <1210704221@qq.com>
2020-06-09 17:15:07 +0800

    初始化计算器项目的基本结构

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>Hello
+  <my-addition></my-addition>
+  <p>------------------------------</p>
+  <my-subtraction></my-subtraction>
+  </div>
+</template>
+
+<script>
+import Addition from './components/Addition.vue'
+import Subtraction from './components/Subtraction.vue'
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    'my-addition': Addition,
+    'my-subtraction': Subtraction
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
